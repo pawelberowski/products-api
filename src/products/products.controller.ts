f0530a6217e345e2a36677415baf6e0cb2e3945f
pawelberowski <pawelberowski@gmail.com>
2023-11-13 09:43:02 +0100

    chore(Products) added CRUD for products

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,5 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { ProductDto } from './product.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -8,5 +18,24 @@ export class ProductsController {
   @Get()
   getAll() {
     return this.productsService.getAll();
+  }
+  @Get(':id')
+  getById(@Param('id', ParseIntPipe) id: number) {
+    return this.productsService.getById(id);
+  }
+
+  @Post()
+  create(@Body() product: ProductDto) {
+    return this.productsService.create(product);
+  }
+
+  @Delete(':id')
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.productsService.delete(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id', ParseIntPipe) id: number, @Body() product: ProductDto) {
+    return this.productsService.update(id, product);
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../database/prisma.service';
+import { ProductNotFoundException } from './product-not-found.exception';
+import { PrismaError } from '../../prisma/prisma-error.enum';
+import { ProductDto } from './product.dto';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class ProductsService {
@@ -7,5 +11,63 @@ export class ProductsService {
 
   getAll() {
     return this.prismaService.product.findMany();
+  }
+
+  async getById(id: number) {
+    const product = await this.prismaService.product.findUnique({
+      where: {
+        id,
+      },
+    });
+    if (!product) {
+      throw new ProductNotFoundException(id);
+    }
+    return product;
+  }
+
+  create(product: ProductDto) {
+    return this.prismaService.product.create({
+      data: product,
+    });
+  }
+
+  async delete(id: number) {
+    try {
+      return await this.prismaService.product.delete({
+        where: {
+          id,
+        },
+      });
+    } catch (error) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === PrismaError.RecordDoesNotExist
+      ) {
+        throw new ProductNotFoundException(id);
+      }
+      throw error;
+    }
+  }
+
+  async update(id: number, product: ProductDto) {
+    try {
+      return await this.prismaService.product.update({
+        data: {
+          ...product,
+          id: undefined,
+        },
+        where: {
+          id,
+        },
+      });
+    } catch (error) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === PrismaError.RecordDoesNotExist
+      ) {
+        throw new ProductNotFoundException(id);
+      }
+      throw error;
+    }
   }
 }
