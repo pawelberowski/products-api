13837c67092e84532aeae44bed753108952e586b
pawelberowski <pawelberowski@gmail.com>
2023-11-13 10:49:11 +0100

    feat(migration) added quantity to isInStock migration

---
+++ prisma/migrations/20231113093624_replace_quantity_with_is_in_stock/migration.sql
@@ -0,0 +1,5 @@
+
+-- AlterTable
+ALTER TABLE "Product" RENAME CONSTRAINT "Products_pkey" TO "Product_pkey",
+ALTER TABLE "Product" ALTER "quantity" TYPE bool USING CASE WHEN "quantity"=0 THEN FALSE ELSE TRUE END;
+ALTER TABLE "Product" ALTER COLUMN "quantity" RENAME "isInStock";
